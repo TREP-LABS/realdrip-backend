7e86fe59f8703c6c7fd67486d301f2770cfc8208
Osivwi Okiti <osivwiokiti@gmail.com>
2019-09-15 18:33:01 +0100

    UPDATE verify device controller and test

--- src/http/controllers/device.js
+++ src/http/controllers/device.js
@@ -1,23 +1,16 @@
 import deviceModel from '../../db/deviceModel';
 import { check, validationResult } from 'express-validator';
 
-// Flow
-// Validate request
-// Verify device
-// Return Response
-let validation = (method) => {
+const validation = (method) => {
     switch (method) {
       case 'verifyDevice': {
-       return [
-        check('deviceId').not().isEmpty().withMessage('deviceId can\'t be empty')
-      ]
+        return [ check('deviceId').not().isEmpty().withMessage('deviceId can\'t be empty') ]
       }
     }
   }
-let verifyDevice  = async (req, res)=>{
-  console.log(req.body.deviceId);
-  
-    const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+const verifyDevice  = async (req, res)=>{
+    console.log(req.body.deviceId);  
+    let errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
     if (!errors.isEmpty()) {
       res.status(422).json({
         status: false,
@@ -25,33 +18,39 @@ let verifyDevice  = async (req, res)=>{
         errors: errors.array() 
       });
     }
-    let deviceId = req.body.deviceId;   
+    // Get the device ID from the request body 
+    let deviceId = req.body.deviceId;
+    // Check if device exits on firebase. returns passed or failed 
     let verification = await deviceModel.verifyDevice(deviceId);
+    // check the value returned by the verification  
     if (verification === "passed") {
       deviceModel.connect.then(
         (client)=>{
+          // Checking if device has previously been verified.
           deviceModel.read(client, "devices", {deviceId:deviceId}, (Response, connection)=> {
             if (Response.length >= 1 ) {
               connection.close();
-              return res.json({
+              return res.status(422).json({
                 device_id : deviceId,
                 status: "failed",
                 message: "Device already verified"
               });
             }else if ( Response.length == 0 ) {
+              // If device has not been previously 
+              // Add device as been verified then return appropiate resonse
               deviceModel.connect.then(
                   (db, client)=>{
                     deviceModel.create(db, [{deviceId : deviceId}], (result, connection)=>{
                       connection.close();
-                      return res.json({
+                      return res.status(201).json({
                         device : result,
                         status: "Passed",
                         message: "Device verified successfully"
                       });
                    })
                 }).catch((err)=>{
-                  console.log("An error occured");    
-                })
+                  console.log(err);    
+                });
             } 
           })
         })
