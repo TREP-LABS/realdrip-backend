9501e4d765b92257711406671736eacbda46135e
Osivwi Okiti <osivwiokiti@gmail.com>
2019-09-20 12:25:57 +0100

    FIX build error due to async request

--- src/tests/device.test.js
+++ src/tests/device.test.js
@@ -3,13 +3,19 @@ import app from '../http/app';
 
 const request = supertest(app);
 it('gets the version endpoint', async (done) => {
-  const response = await request.get('/api/version');
-  expect(response.status).toBe(200);
-  done();
+  request
+    .get('/api/version')
+    .end((err, res) => {
+      expect(res.status).toBe(200);
+      done();
+    });
 });
 it('gets the verify device endpoint', async (done) => {
-  const response = await request
-    .post('/api/device/verify').send({ deviceId: 'rd-2002' });
-  expect(response.status).toBe(422);
-  done();
+  request
+    .post('/api/device/verify')
+    .send({ deviceId: 'rd-2002' })
+    .end((err, res) => {
+      expect(res.status).toBe(422);
+      done();
+    });
 }, 500000);
