3a8fac55271927978edd23e51c7db0bbeec6e78f
Tunmise Ogunniyi <ogunniyitunmise@gamil.com>
2019-09-23 11:00:10 +0100

    Enable CORS for browser clients

--- src/http/app.js
+++ src/http/app.js
@@ -7,6 +7,15 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Enabling CORS for browser clients
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
+
 app.use('/api', routes);
 
 export default app.listen(config.port, () => {
