504964440905fe8f1b404959c16bd211391cd41e
Tunmise Ogunniyi <ogunniyitunmise@gamil.com>
2019-09-25 11:56:25 +0100

    Use email service to send email address verification mail

--- src/services/user.js
+++ src/services/user.js
@@ -1,11 +1,13 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import db from '../db';
+import emailService from './email';
+import config from './config';
 
 const createAdminUser = async (data) => {
   const {
     name, email, location, password,
   } = data;
-  const hashedPassword = await bcrypt.hash(password, 10);
   const lowerCaseEmail = email.toLowerCase();
   const alreadyExistingUser = await db.adminUser.getAdminUserByEmail(lowerCaseEmail);
   if (alreadyExistingUser) {
@@ -13,6 +15,7 @@ const createAdminUser = async (data) => {
     error.httpStatusCode = 409;
     throw error;
   }
+  const hashedPassword = await bcrypt.hash(password, 10);
   const adminUser = await db.adminUser.createAdminUser({
     name,
     email: lowerCaseEmail,
@@ -21,6 +24,15 @@ const createAdminUser = async (data) => {
     confirmed: false,
     deviceCount: 0,
   });
+  const confirmationToken = jwt.sign({ email, action: 'confirmation' }, config.jwtSecrete);
+  const confirmationUrl = `${config.appUrl}/api/users/confirm?token=${confirmationToken}`;
+  // NOTE: The actions below is asynchronous, however, I don't need to wait for it to complete
+  // before sending response to the user.
+  emailService.sendEmailAddresValidation({ name, email }, confirmationUrl)
+    .catch((err) => {
+      console.log('Error sending email to user with email: ', email);
+      console.log(err);
+    });
   return {
     // eslint-disable-next-line no-underscore-dangle
     id: adminUser._id,
