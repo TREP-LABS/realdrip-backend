41d9618832e962830f27afb4a0806182c41aaace
Osivwi Okiti <osivwiokiti@gmail.com>
2019-09-30 12:15:06 +0100

    UPDATE simplification and abstraction of firebase connection details

--- src/db/models/config.js
+++ src/db/models/config.js
@@ -1,31 +1,11 @@
-import firebase from 'firebase-admin';
 import dotenv from 'dotenv';
-import serviceAccount from './fireboy-79d85-firebase-adminsdk-eq2r2-07f67da5cb.json';
 
 dotenv.config();
 
 export default {
   environment: process.env.NODE_ENV || 'development',
-  firebase_config: {
-    test: {
-      credential: firebase.credential.cert(serviceAccount),
-      apiKey: process.env.FIREBASE_TEST_APIKEY,
-      authDomain: process.env.FIREBASE_TEST_AUTHDOMAIN,
-      databaseURL: process.env.FIREBASE_TEST_DATABASEURL,
-      projectId: process.env.FIREBASE_TEST_PROJECTID,
-      storageBucket: process.env.FIREBASE_TEST_STORAGEBUCKET,
-      messagingSenderId: process.env.FIREBASE_TEST_MESSAGINGSENDERID,
-      appId: process.env.FIREBASE_TEST_APPID,  
-    },
-    development: {
-      credential: firebase.credential.cert(serviceAccount),
-      apiKey: process.env.FIREBASE_DEV_APIKEY,
-      authDomain: process.env.FIREBASE_DEV_AUTHDOMAIN,
-      databaseURL: process.env.FIREBASE_DEV_DATABASEURL,
-      projectId: process.env.FIREBASE_DEV_PROJECTID,
-      storageBucket: process.env.FIREBASE_DEV_STORAGEBUCKET,
-      messagingSenderId: process.env.FIREBASE_DEV_MESSAGINGSENDERID,
-      appId: process.env.FIREBASE_DEV_APPID,  
-    },
+  fbUrl: {
+    test: process.env.FIREBASE_TEST_DATABASEURL,
+    development: process.env.FIREBASE_DEV_DATABASEURL,
   }
 };
--- src/db/models/device.js
+++ src/db/models/device.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase-admin';
 import config from './config';
+import serviceAccount from './fireboy-79d85-firebase-adminsdk-eq2r2-07f67da5cb.json';
+
 class Device {
   constructor(model) {
     this.Model = model;
-    this.fb = firebase.initializeApp(config.firebase_config[config.environment]);
+    this.fb = firebase.initializeApp({
+      credential: firebase.credential.cert(serviceAccount),
+      databaseURL: config.fbUrl[config.environment],
+    });
   }
   async getDevice (deviceId) {
     return await this.fb.database().ref('/fireboy/devices/')
