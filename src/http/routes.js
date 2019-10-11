b5776608c99de7934f51455424f998f55e0b2676
Osivwi Okiti <osivwiokiti@gmail.com>
2019-10-11 16:45:00 +0100

    FIX linting errors

--- src/http/routes.js
+++ src/http/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import controllers from './controllers';
-import auth from './middleware/auth';
+import authMiddleware from './middleware/authMiddleware';
 
 const router = express.Router();
 
@@ -9,6 +9,6 @@ router.get('/health', (req, res) => res.json({ status: 'I am alive' }));
 router.post('/hospital', controllers.user.createAdminUser);
 router.get('/hospital/confirmEmail', controllers.user.confirmUserAccount);
 router.post('/users/login', controllers.user.login);
-router.get('/device/:deviceId', auth, controllers.device.getSingleDevice);
+router.get('/device/:deviceId', authMiddleware, controllers.device.getSingleDevice);
 
 export default router;
