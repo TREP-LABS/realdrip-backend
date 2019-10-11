864c89df2946771a81ec35c1a4a873fe12d932d7
Osivwi Okiti <osivwiokiti@gmail.com>
2019-10-11 16:14:45 +0100

    FIX PR reviews

--- src/http/middleware/Auth.js
+++ src/http/middleware/Auth.js
@@ -4,51 +4,25 @@ import db from '../../db';
 
 dotenv.config();
 
-// import/prefer-default-export
-const Auth = async (req, res, next) => {
-  const token = req.header('Authorization');
+const auth = async (req, res, next) => {
+  const token = req.header('req-token');
 
-  if (typeof token === 'undefined') {
-    return res.status(401).json({ success: false, message: 'unauthenticated: header not found' });
+  if (!token || typeof token === 'undefined') {
+    return res.status(401).json({ success: false, message: 'req-token not found in request header' });
   }
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const { id } = decoded;
-    const { type } = decoded;
+    const decoded = jwt.verify(token, process.env.JWT_SECRETE);
+    const { id, type } = decoded;
     // eslint-disable-next-line no-underscore-dangle
     const user = await db.users.getUser({ _id: id }, type);
     if (!user) {
       return res.status(404).json({ success: false, message: 'Invalid user' });
     }
-    const basic = {
-      // eslint-disable-next-line no-underscore-dangle
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-    };
-    const userProfile = {
-
-      hospital_admin: {
-        ...basic,
-        location: user.location,
-        // eslint-disable-next-line no-underscore-dangle
-        hospitalId: user._id,
-      },
-      ward_user: {
-        ...basic,
-        hospitalId: user.hospitalId,
-      },
-      nurse_user: {
-        ...basic,
-        hospitalId: user.hospitalId,
-        wardId: user.wardId,
-      },
-    };
-
-    res.locals.user = userProfile[type];
+    res.locals.user = user;
+    res.locals.userType = type;
   } catch (err) {
-    return res.status(403).json({ success: false, message: `${err.name}: ${err.message}` });
+    return res.status(401).json({ success: false, message: 'Unable to authenticate token' });
   }
   return next();
 };
-export default Auth;
+export default auth;
