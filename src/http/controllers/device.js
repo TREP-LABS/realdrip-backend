1ed010600204cfe66b3b16b395f6adb61711b29b
Osivwi Okiti <osivwiokiti@gmail.com>
2019-10-14 13:14:06 +0100

    ADD logger

--- src/http/controllers/device.js
+++ src/http/controllers/device.js
@@ -3,15 +3,18 @@ import deviceValidation from '../validations/device';
 
 const getSingleDevice = async (req, res) => {
   const { deviceId } = req.params;
-  const { user, userType } = res.locals;
-
+  const { user, userType, log } = res.locals;
+  log.debug('Executing the getAllDevice controller');
   try {
-    const device = await deviceService.getSingleDevice(deviceId, user, userType);
+    const device = await deviceService.getSingleDevice(deviceId, user, userType, log);
+    log.debug('getSingleDevice service executed without error, sending back a success response');
     return res.status(200).json({ success: true, message: 'Device found', data: device });
   } catch (err) {
     if (err.httpStatusCode) {
+      log.debug('getSingleDevie service failed with an http status code, sending back a failure response');
       return res.status(err.httpStatusCode).json({ success: false, message: err.message });
     }
+    log.error(err, 'getSingleDevice service failed without an http status code');
     return res.status(500).json({ success: false, message: 'Error getting device' });
   }
 };
--- src/services/device.js
+++ src/services/device.js
@@ -1,7 +1,9 @@
 import db from '../db';
 
-const getSingleDevice = async (deviceId, user, userType) => {
+const getSingleDevice = async (deviceId, user, userType, log) => {
+  log.debug('Executing getSingleDevice service');
   try {
+    log.debug('Gathering filter parameters for getting device');
     const deviceMatch = {
       // eslint-disable-next-line no-underscore-dangle
       _id: deviceId,
@@ -10,13 +12,15 @@ const getSingleDevice = async (deviceId, user, userType) => {
       // eslint-disable-next-line no-underscore-dangle
       wardId: userType === 'ward_user' ? user._id : user.wardId,
     };
-
+    log.debug('Fetching device from database');
     const device = await db.device.getSingleDevice(deviceMatch);
+    log.debug('Returning device to user');
     return { device };
   } catch (err) {
+    log.debug('Unable to get device. throwing error');
     const error = new Error(`Unable to get device with id: ${deviceId}`);
     error.httpStatusCode = 404;
     throw error;
   }
 };
-module.exports = { getSingleDevice };
+export default { getSingleDevice };
