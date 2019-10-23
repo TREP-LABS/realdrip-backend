42f16fa08bdbb3adc5c3e781310a2069a6262861
Osivwi Okiti <osivwiokiti@gmail.com>
2019-10-23 13:48:33 +0100

    UPDATE rebased with master & fixed review issues

--- src/http/validations/device.js
+++ src/http/validations/device.js
@@ -16,12 +16,24 @@ const getSingleDevice = (req, res, next) => {
 
 const updateDevice = (req, res, next) => {
   const { deviceId } = req.params;
+  const { label, wardId, hospitalId } = req.body;
 
   const fieldErrors = new FieldErrors();
+  if (label) {
+    if (typeof (label) !== 'string') fieldErrors.addError('label', 'label is a string');
+  }
+  if (wardId) {
+    if (!db.validResourceId(wardId)) fieldErrors.addError('wardId', 'wardId is not valid');
+  }
+  if (hospitalId) {
+    if (!db.validResourceId(hospitalId)) fieldErrors.addError('hospitalId', 'hospitalId is not valid');
+  }
+  if (!deviceId) fieldErrors.addError('deviceId', 'deviceId is required');
+  if (!db.validResourceId(deviceId)) fieldErrors.addError('deviceId', 'deviceId is not valid');
 
-  if (!db.validResourceId(deviceId)) fieldErrors.addError('deviceId', 'deviceId should be a ');
-  if (!deviceId || typeof (deviceId) !== 'string') fieldErrors.addError('deviceId', 'deviceId is a required string');
-  if (req.body.constructor === Object && Object.keys(req.body).length === 0) fieldErrors.addError('request', 'Request body can\'t be empty');
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    return res.status(400).json({ success: false, message: 'Invalid request: All fields can\'t be empty' });
+  }
 
   if (fieldErrors.count > 0) {
     return res.status(400).json({ success: false, message: 'Invalid request', errors: fieldErrors.errors });
