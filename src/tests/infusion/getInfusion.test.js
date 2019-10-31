014dd7b5752381b31c074669a0425ffc61a8e9ab
Tunmise Ogunniyi <ogunniyitunmise@gamil.com>
2019-10-31 10:21:51 +0100

    Add test for getSingle infusion endpoint

--- src/tests/infusion/getInfusion.test.js
+++ src/tests/infusion/getInfusion.test.js
@@ -45,7 +45,7 @@ const testCases = [
   // This test case is added here even when the get single infusion endpoint is not implemented
   // just to demonstrate how to go about creating a test case for getting single infusion
   {
-    title: 'should return 404 for get single infusions',
+    title: 'should get a single infusion',
     request: context => ({
       body: {},
       endpoint: `/api/infusion/${context.infusionId}`,
@@ -54,8 +54,22 @@ const testCases = [
       },
     }),
     response: {
-      status: 404,
-      body: {},
+      status: 200,
+      body: {
+        success: true,
+        message: 'Infusion found',
+        data: {
+          // @todo: Having _id instead of id here is a bug, it should be fixed
+          _id: expect.any(String),
+          startVolume: infusion.startVolume,
+          stopVolume: infusion.stopVolume,
+          patientName: infusion.patientName,
+          doctorsInstruction: infusion.doctorsInstruction,
+          deviceId: infusion.deviceId,
+          wardId: expect.any(String),
+          hospitalId: expect.any(String),
+        },
+      },
     },
   },
 ];
@@ -75,7 +89,8 @@ beforeAll(() => {
       if (!res.body || !res.body.success) {
         throw Error('Infusion creation failed, all other tests in this suite is also expected to fail');
       }
-      infusionId = res.body.data.id;
+      // @todo: Having _id instead of id here is a bug, it should be fixed
+      infusionId = res.body.data._id;
     });
 });
 
