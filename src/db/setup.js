07575cdf4ace2e2eed336a53783d2135de9c1bc9
Osivwi Okiti <osivwiokiti@gmail.com>
2019-11-04 21:10:36 +0100

    FIX console warning when app starts

--- src/db/setup.js
+++ src/db/setup.js
@@ -5,7 +5,9 @@ const dbUrl = config.dbUrl[config.environment];
 
 export default () => {
   console.info(`Start setting up database for ${config.environment} environment`);
-  mongoose.connect(dbUrl, { useNewUrlParser: true, useFindAndModify: false });
+  mongoose.connect(dbUrl, {
+    useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, useCreateIndex: true,
+  });
   mongoose.Promise = global.Promise;
   console.info('Finished setting up database');
 };
